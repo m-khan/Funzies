e66074be3e118411f6120c6cc2a406aa41e8a10f
Michael Han <mhan.sensei@gmail.com>
2017-02-16 20:37:25 -0800

    Added simple UI to LocalWatchBot

--- KhanBot/src/evebot/LocalWatchBot.java
+++ KhanBot/src/evebot/LocalWatchBot.java
@@ -1,46 +1,65 @@
 package evebot;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.MouseInfo;
 import java.awt.Point;
-import java.awt.PointerInfo;
 import java.awt.Robot;
 import java.awt.Toolkit;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class LocalWatchBot {
 
 	public static void main(String[] args) throws Exception{
-		System.out.println("Set local position...");
+		System.out.println("Set local position...                                        ");
 
-		PointerInfo pointer;
-        pointer = MouseInfo.getPointerInfo();
-
+		JFrame frame = new JFrame("Local Watch Bot");
+		
+		JLabel label = new JLabel("PUT MOUSE OVER LOCAL TOP                  ");
+		frame.setLayout(new BorderLayout());
+		frame.add(label, BorderLayout.NORTH);
+		JCheckBox checkbox = new JCheckBox("SHUT UP");
+		frame.add(checkbox, BorderLayout.SOUTH);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		
         Robot robot = new Robot();
         robot.delay(5000);
         Toolkit.getDefaultToolkit().beep();
 
         Point localTop = MouseInfo.getPointerInfo().getLocation();
         System.out.println("Local top set at: " + localTop);
-
+        label.setText("Local top set at: " + localTop);
+        
         robot.delay(2000);
         Toolkit.getDefaultToolkit().beep();
         
         Point localBottom = MouseInfo.getPointerInfo().getLocation();
         System.out.println("Local bottom set at: " + localBottom);
+        label.setText("WATCHING...");
         
         Point cP = new Point(localTop); // currentPosition
+        boolean detected = false;
         
         while(true) {
             Color color = robot.getPixelColor((int)cP.getX(), (int)cP.getY());
             if(color.getGreen() + color.getBlue() < color.getRed()) {
                 System.out.println("Hostile Detected");
-                Toolkit.getDefaultToolkit().beep();
+                if(!checkbox.isSelected()) Toolkit.getDefaultToolkit().beep();
                 System.out.println("Color: "+ color);
+                detected = true;
             }
             robot.delay(2);
             
             if(cP.getY() > localBottom.getY()){
             	cP.y = localTop.y;
+                if(detected) label.setText("Hostile Detected!");
+                else label.setText("WATCHING...");
+            	detected = false;
             }
             else {
             	cP.y = cP.y + 3;
